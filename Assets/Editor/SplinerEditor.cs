9968ca47eff4e797a63581e1312fc00349c241ba
Tobias Heinemann <tobyhein@gmail.com>
2017-02-16 01:34:54 -0800

    Added Add Segment and Flatten Methods
    Added Add Start and Add End Segment Methods
    Added Flatten X,Y,Z Methods
    Edited Clear All
    Edited default
    Converted everything to be Transformable (responds to parent position,
    rotation, scale)
    Code cleanup
    Inspector edits

--- Assets/Editor/SplinerEditor.cs
+++ Assets/Editor/SplinerEditor.cs
@@ -10,35 +10,79 @@ namespace Softdrink{
 			DrawDefaultInspector();
 
 			SplinerBase t = target as SplinerBase;
-			if(GUILayout.Button("Reset All Segments")) t.ResetAllSegments();
+			Undo.RecordObject(t, "Spliner Edit");
+
+			GUILayout.Space(10);
+
+			EditorGUILayout.LabelField("Add Segments", EditorStyles.boldLabel);
+			
+			if(GUILayout.Button("Add End Segment")) t.AddNewEnd();
+			if(GUILayout.Button("Add Start Segment")) t.AddNewStart();
+			GUILayout.Space(10);
+
+			EditorGUILayout.LabelField("Flatten", EditorStyles.boldLabel);
+			if(GUILayout.Button("Flatten X Axis")) t.Flatten("X");
+			if(GUILayout.Button("Flatten Y Axis")) t.Flatten("Y");
+			if(GUILayout.Button("Flatten Z Axis")) t.Flatten("Z");
+			GUILayout.Space(10);
+
+			EditorGUILayout.LabelField("Clear", EditorStyles.boldLabel);
+			if(GUILayout.Button("Clear All Segments")) t.ResetAllSegments();
+
+			EditorUtility.SetDirty(t);
 		}
 
 		void OnSceneGUI(){
 			SplinerBase t = target as SplinerBase;
-			Undo.RecordObject(t, "Spline Segment Edit");
+			serializedObject.Update();
+			//Undo.RecordObject(t, "Spline Segment Edit");
+			EditorGUI.BeginChangeCheck();
+
+			Vector3 handlePos = new Vector3(0f,0f,0f);
 
 			// Index through and create handles for startPoint, startTangent, endPoint, endTangent
 			for(int i = 0; i < t.splineSegments.Length; i++){
 				
 
 				// Create handle for startPoint
-				if(t.splineSegments[i].prev == null || !t.enforceContinuousSpline)
-					t.splineSegments[i].startPoint = Handles.PositionHandle(t.splineSegments[i].startPoint, Quaternion.identity);
+				if(t.splineSegments[i].prev == null || !t.enforceContinuousSpline){
+					handlePos = t.splineSegments[i].Transformed(t.splineSegments[i].startPoint);
+					handlePos = Handles.PositionHandle(handlePos, Quaternion.identity);
+					t.splineSegments[i].startPoint = t.splineSegments[i].InverseTransformed(handlePos);
+
+					//t.splineSegments[i].startPoint = Handles.PositionHandle(t.splineSegments[i].startPoint, Quaternion.identity);
+				}
 				else if( i >= 1) t.splineSegments[i].startPoint = t.splineSegments[i-1].endPoint;
 				// Create handle for startTangent
-				if(t.splineSegments[i].prev == null || !t.autoAlignTangents)
-					t.splineSegments[i].startTangent = Handles.PositionHandle(t.splineSegments[i].startTangent, Quaternion.identity);
+				if(t.splineSegments[i].prev == null || !t.autoAlignTangents){
+					handlePos = t.splineSegments[i].Transformed(t.splineSegments[i].startTangent);
+					handlePos = Handles.PositionHandle(handlePos, Quaternion.identity);
+					t.splineSegments[i].startTangent = t.splineSegments[i].InverseTransformed(handlePos);
+
+					//t.splineSegments[i].startTangent = Handles.PositionHandle(t.splineSegments[i].startTangent, Quaternion.identity);
+				}
 				else if(i >= 1) t.splineSegments[i].startTangent = t.splineSegments[i-1].endTangent;
 
 				// Create handle for endPoint
-				t.splineSegments[i].endPoint = Handles.PositionHandle(t.splineSegments[i].endPoint, Quaternion.identity);
+				handlePos = t.splineSegments[i].Transformed(t.splineSegments[i].endPoint);
+				handlePos = Handles.PositionHandle(handlePos, Quaternion.identity);
+				t.splineSegments[i].endPoint = t.splineSegments[i].InverseTransformed(handlePos);
+
+				//t.splineSegments[i].endPoint = Handles.PositionHandle(t.splineSegments[i].endPoint, Quaternion.identity);
+
 				// Create handle for endTangent
-				t.splineSegments[i].endTangent = Handles.PositionHandle(t.splineSegments[i].endTangent, Quaternion.identity);
+				handlePos = t.splineSegments[i].Transformed(t.splineSegments[i].endTangent);
+				handlePos = Handles.PositionHandle(handlePos, Quaternion.identity);
+				t.splineSegments[i].endTangent = t.splineSegments[i].InverseTransformed(handlePos);
+
+				//t.splineSegments[i].endTangent = Handles.PositionHandle(t.splineSegments[i].endTangent, Quaternion.identity);
 
 				
 			}
-			
-			EditorUtility.SetDirty(t);
+
+			//EditorUtility.SetDirty(t);
+			serializedObject.ApplyModifiedProperties();
+			if(EditorGUI.EndChangeCheck()) Undo.RecordObject(t, "Spline Segment Edit");
 		}
 
 	}
--- Assets/SplinerBase.cs
+++ Assets/SplinerBase.cs
@@ -14,6 +14,9 @@ namespace Softdrink{
 
 	[System.Serializable]
 	public class SplineSection{
+		// What is the Transform that this SplineSection is relative to?
+		[HideInInspector]
+		public Transform parent;
 
 		// What value to return if there is an error getting the position
 		private Vector3 nullReturn = new Vector3(-99f,-99f,-99f);
@@ -48,21 +51,26 @@ namespace Softdrink{
 			startTangent = input.startTangent;
 			endPoint = input.endPoint;
 			endTangent = input.endTangent;
+			parent = input.parent;
+		}
+
+		public SplineSection(Transform parentIn){
+			parent = parentIn;
 		}
 
 		public Vector3 GetPositionAt(float t) {
 			if ((t < 0f) && (prev!=null) && prev.IsValid()) {
-				return prev.GetPositionAt(t+1.0f);
+				return Transformed(prev.GetPositionAt(t+1.0f));
 			} else if ((t > 1f) && (next!=null) && next.IsValid()) {
-				return next.GetPositionAt(t-1.0f);
+				return Transformed(next.GetPositionAt(t-1.0f));
 			} else if (IsValid()) {
 				float t2 = t*t;
 				float t3 = t2*t;
 				Vector3 p0 = startPoint, p1 = endPoint, m0 = startTangent - p0, m1 = endTangent - p1;
-				return (p0 * ((2.0f*t3) - (3.0f*t2) + 1.0f))
+				return Transformed((p0 * ((2.0f*t3) - (3.0f*t2) + 1.0f))
 					+ (m0 * (t3 + (-2.0f*t2) + t))
 					+ (p1 * ((-2.0f*t3) + (3.0f*t2)))
-					+ (m1 * (t3 - t2));
+					+ (m1 * (t3 - t2)));
 			} else {
 				return nullReturn;
 			}
@@ -72,20 +80,48 @@ namespace Softdrink{
 			return (startPoint != nullReturn) && (startTangent != nullReturn) && (endPoint != nullReturn) && (endTangent != nullReturn);
 		}
 
+		public Vector3 Transformed(Vector3 input){
+			Vector3 output = new Vector3(input.x, input.y, input.z);
+			output += parent.position;
+			output = parent.rotation * output;
+			output = new Vector3(output.x * parent.lossyScale.x, output.y * parent.lossyScale.y, output.z * parent.lossyScale.z);
 
+			
+
+			return output;
+			//return parent.TransformPoint(input);
+
+		}
+
+		public Vector3 InverseTransformed(Vector3 input){
+			Vector3 output = new Vector3(input.x, input.y, input.z);
+			output = new Vector3(output.x / parent.lossyScale.x, output.y / parent.lossyScale.y, output.z/parent.lossyScale.z);
+			//output = new Vector3(output.x / parent.rotation.x, output.y / parent.rotation.y, output.z / parent.rotation.z);
+			output = Quaternion.Inverse(parent.rotation) * output;
+			output -= parent.position;
+
+			
+
+			return output;
+			//return parent.InverseTransformPoint(input);
+		}
 
 		public void DrawGizmos(){
 			SetSegmentStatus();
 
 			if(IsValid()) {
 				Gizmos.color = Color.grey;
-				Gizmos.DrawLine(startPoint, startTangent);
-				Gizmos.DrawLine(endPoint, endTangent);
+				Gizmos.DrawLine(Transformed(startPoint), Transformed(startTangent));
+				Gizmos.DrawLine(Transformed(endPoint), Transformed(endTangent));
 				for(int i = 0; i < gizmoDetail; i++) {
 					Gizmos.DrawLine(GetPositionAt(((float)i)/(float)gizmoDetail),
 					                GetPositionAt(((float)(i+1))/(float)gizmoDetail));
 				}
 			}
+
+			Gizmos.color = Color.grey;
+			Gizmos.DrawWireSphere(Transformed(endPoint), gizmoSphereSize);
+			if(prev == null) Gizmos.DrawWireSphere(Transformed(startPoint), gizmoSphereSize);
 
 			//Debug.Log(forceContinuous);
 		}
@@ -95,8 +131,8 @@ namespace Softdrink{
 
 			if(IsValid()) {
 				Gizmos.color = Color.yellow;
-				Gizmos.DrawLine(startPoint, startTangent);
-				Gizmos.DrawLine(endPoint, endTangent);
+				Gizmos.DrawLine(Transformed(startPoint), Transformed(startTangent));
+				Gizmos.DrawLine(Transformed(endPoint), Transformed(endTangent));
 				Gizmos.color = Color.white;
 				int gizmoDetailTemp = gizmoDetail*2;
 				for(int i = 0; i < gizmoDetailTemp; i++) {
@@ -107,25 +143,25 @@ namespace Softdrink{
 			// If this is a start point, draw a colored wire sphere at the start point, otherwise use white
 			if(prev == null){
 				Gizmos.color = Color.green;
-				Gizmos.DrawWireSphere(startPoint, gizmoSphereSize);
+				Gizmos.DrawWireSphere(Transformed(startPoint), gizmoSphereSize);
 			}else{
 				Gizmos.color = Color.white;
-				Gizmos.DrawWireSphere(startPoint, gizmoSphereSize);
+				Gizmos.DrawWireSphere(Transformed(startPoint), gizmoSphereSize);
 			}
 
 			// If this is a end point, draw a colored wire sphere at the end point, otherwise use white
 			if(next == null){
 				Gizmos.color = Color.red;
-				Gizmos.DrawWireSphere(endPoint, gizmoSphereSize);
+				Gizmos.DrawWireSphere(Transformed(endPoint), gizmoSphereSize);
 			}else{
 				Gizmos.color = Color.white;
-				Gizmos.DrawWireSphere(endPoint, gizmoSphereSize);
+				Gizmos.DrawWireSphere(Transformed(endPoint), gizmoSphereSize);
 			}
 
 			// Draw spheres at tangent handles
 			Gizmos.color = Color.yellow;
-			Gizmos.DrawSphere(startTangent, gizmoSphereSize/1.5f);
-			Gizmos.DrawSphere(endTangent, gizmoSphereSize/1.5f);
+			Gizmos.DrawWireSphere(Transformed(startTangent), gizmoSphereSize/1.5f);
+			Gizmos.DrawWireSphere(Transformed(endTangent), gizmoSphereSize/1.5f);
 		}
 
 		public void SetSegmentStatus(){
@@ -213,12 +249,23 @@ namespace Softdrink{
 		private float gizmoSphereSize = 0.025f;
 
 		void OnDrawGizmos() {
+			// Draw a larger sphere for the main Transform parent
+			Gizmos.color = Color.grey;
+			Gizmos.DrawWireSphere(transform.position, gizmoSphereSize * 3f);
+			Gizmos.DrawWireSphere(transform.position, gizmoSphereSize * 2f);
+
+
 			for(int i = 0; i < splineSegments.Length; i++){
 				splineSegments[i].DrawGizmos();
 			}
 		}
 
 		void OnDrawGizmosSelected() {
+			// Draw a larger sphere for the main Transform parent
+			Gizmos.color = Color.white;
+			Gizmos.DrawWireSphere(transform.position, gizmoSphereSize * 3f);
+			Gizmos.DrawWireSphere(transform.position, gizmoSphereSize * 2f);
+
 			for(int i = 0; i < splineSegments.Length; i++){
 				splineSegments[i].DrawGizmosSelected();
 			}
@@ -229,8 +276,15 @@ namespace Softdrink{
 			// If the array doesn't exist bail out
 			if(splineSegments == null) return;
 
+			UpdateSegments();
+		}
+
+		public void UpdateSegments(){
 			// Otherwise index through and assign the prev and next instances automatically, update positions, set status, etc.
 			for(int i = 0; i < splineSegments.Length; i++){
+				// Set the parent
+				splineSegments[i].parent = transform;
+
 				// Assign prev references if not at beginning of list
 				if(i < 1) splineSegments[i].prev = null;
 				else splineSegments[i].prev = splineSegments[i-1];
@@ -264,6 +318,118 @@ namespace Softdrink{
 			splineSegments = null;
 
 			splineSegments = new SplineSection[1]; 
+			splineSegments[0] = new SplineSection(transform);
+			splineSegments[0].endTangent = new Vector3(1f,0f,1f);
+			splineSegments[0].endPoint = new Vector3(0.5f, 0f, 0.5f);
+			splineSegments[0].startPoint = new Vector3(-0.5f, 0f, -0.5f);
+			splineSegments[0].startTangent = new Vector3(-1f, 0f, -1f);
+		}
+
+		// Extend the length of the spline by one in the Forward direction
+		public void AddNewEnd(){
+			// Create a temp array of Spline Sections and populate it with the current content of the spline Segments array
+			SplineSection[] temp = new SplineSection[splineSegments.Length];
+			for(int i = 0; i < temp.Length; i++){
+				temp[i] = new SplineSection(splineSegments[i]);
+			}
+
+			// Unset and reinitialize the current array
+			splineSegments = null;
+			splineSegments = new SplineSection[temp.Length + 1];
+
+			// Get the old array contents from the temp buffer
+			for(int i = 0; i < temp.Length; i++){
+				splineSegments[i] = new SplineSection(temp[i]);
+			}
+
+			int leng = splineSegments.Length;
+
+			// Create a new SplineSection and set it based on the current EndPoint
+			splineSegments[leng - 1] = new SplineSection(splineSegments[leng - 2]);
+			splineSegments[leng - 1].startPoint = new Vector3(splineSegments[leng - 2].endPoint.x, splineSegments[leng - 2].endPoint.y, splineSegments[leng - 2].endPoint.z);
+			splineSegments[leng - 1].startTangent = new Vector3(splineSegments[leng - 2].endTangent.x, splineSegments[leng - 2].endTangent.y, splineSegments[leng - 2].endTangent.z);
+			// The direction and endTangent of the new segment should be based off of the normalized local space endTangent of the previous segment
+			Vector3 normtan = new Vector3(splineSegments[leng - 2].endPoint.x - -splineSegments[leng - 2].endTangent.x, splineSegments[leng - 2].endPoint.y - -splineSegments[leng - 2].endTangent.y, splineSegments[leng - 2].endPoint.z - -splineSegments[leng - 2].endTangent.z);
+			normtan.Normalize();
+			normtan *= Vector3.Distance(splineSegments[leng - 2].startPoint, splineSegments[leng-2].endPoint);
+
+			splineSegments[leng - 1].endPoint = new Vector3(splineSegments[leng - 1].startPoint.x + normtan.x, splineSegments[leng - 1].startPoint.y + normtan.y, splineSegments[leng - 1].startPoint.z + normtan.z);
+			splineSegments[leng - 1].endTangent = new Vector3(splineSegments[leng - 1].endPoint.x + normtan.x, splineSegments[leng - 1].endPoint.y + normtan.y, splineSegments[leng - 1].endPoint.z + normtan.z);
+
+			// Set status of all spline segments
+			UpdateSegments();
+		}
+
+		// Extend the length of the spline by one in the Backward direction
+		public void AddNewStart(){
+			// Create a temp array of Spline Sections and populate it with the current content of the spline Segments array
+			SplineSection[] temp = new SplineSection[splineSegments.Length];
+			for(int i = 0; i < temp.Length; i++){
+				temp[i] = new SplineSection(splineSegments[i]);
+			}
+
+			// Unset and reinitialize the current array
+			splineSegments = null;
+			splineSegments = new SplineSection[temp.Length + 1];
+
+			// Get the old array contents from the temp buffer
+			for(int i = 0; i < temp.Length; i++){
+				splineSegments[i+1] = new SplineSection(temp[i]);
+			}
+
+			//int leng = 0;
+
+			// Create a new SplineSection and set it based on the current StartPoint
+			splineSegments[0] = new SplineSection(splineSegments[1]);
+			splineSegments[0].endPoint = new Vector3(splineSegments[1].startPoint.x, splineSegments[1].startPoint.y, splineSegments[1].startPoint.z);
+			splineSegments[0].endTangent = new Vector3(splineSegments[1].startTangent.x, splineSegments[1].startTangent.y, splineSegments[1].startTangent.z);
+			// The direction and startTangent of the new segment should be based off of the normalized local space startTangent of the next segment
+			Vector3 normtan = new Vector3(splineSegments[1].startPoint.x - -splineSegments[1].startTangent.x, splineSegments[1].startPoint.y - -splineSegments[1].startTangent.y, splineSegments[1].startPoint.z - -splineSegments[1].startTangent.z);
+			normtan.Normalize();
+			normtan *= Vector3.Distance(splineSegments[1].startPoint, splineSegments[1].endPoint);
+
+			splineSegments[0].startPoint = new Vector3(splineSegments[0].endPoint.x + normtan.x, splineSegments[0].endPoint.y + normtan.y, splineSegments[0].endPoint.z + normtan.z);
+			normtan *= 0.25f;
+			splineSegments[0].startTangent = new Vector3(splineSegments[0].startPoint.x + normtan.x, splineSegments[0].startPoint.y + normtan.y, splineSegments[0].startPoint.z + normtan.z);
+
+			// Set status of all spline segments
+			UpdateSegments();
+		}
+
+		// Flatten a specified axis
+		public void Flatten(string axis){
+			if(axis == "X"){
+				for(int i = 0; i < splineSegments.Length; i++){
+					SplineSection temp = splineSegments[i];
+					temp.startPoint = new Vector3(0f, temp.startPoint.y, temp.startPoint.z);
+					temp.startTangent = new Vector3(0f, temp.startTangent.y, temp.startTangent.z);
+					temp.endPoint = new Vector3(0f, temp.endPoint.y, temp.endPoint.z);
+					temp.endTangent = new Vector3(0f, temp.endTangent.y, temp.endTangent.z);
+					splineSegments[i] = new SplineSection(temp);
+				}
+			}
+			if(axis == "Y"){
+				for(int i = 0; i < splineSegments.Length; i++){
+					SplineSection temp = splineSegments[i];
+					temp.startPoint = new Vector3(temp.startPoint.x, 0f, temp.startPoint.z);
+					temp.startTangent = new Vector3(temp.startPoint.x, 0f, temp.startTangent.z);
+					temp.endPoint = new Vector3(temp.endPoint.x, 0f, temp.endPoint.z);
+					temp.endTangent = new Vector3(temp.endTangent.x, 0f, temp.endTangent.z);
+					splineSegments[i] = new SplineSection(temp);
+				}
+			}
+			if(axis == "Z"){
+				for(int i = 0; i < splineSegments.Length; i++){
+					SplineSection temp = splineSegments[i];
+					temp.startPoint = new Vector3(temp.startPoint.x, temp.startPoint.y, 0f);
+					temp.startTangent = new Vector3(temp.startPoint.x, temp.startTangent.y, 0f);
+					temp.endPoint = new Vector3(temp.endPoint.x, temp.endPoint.y, 0f);
+					temp.endTangent = new Vector3(temp.endTangent.x, temp.endTangent.y, 0f);
+					splineSegments[i] = new SplineSection(temp);
+				}
+			}
+
+
 		}
 
 	}
